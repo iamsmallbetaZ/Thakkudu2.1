de71eaab4fc26a6f72313bd5b7a7bf6883377dee
I4M BetA Z <94156760+I4MBetAZ@users.noreply.github.com>
2022-03-23 10:52:10 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,11 +1,14 @@
 let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
     let haruno = `┌ 「 𝐺𝐼𝑇 & 𝑊𝐴 𝐺𝑅𝑈𝑃 𝐿𝐼𝑁𝐾」
-    ᵀʰᵃᵏᵏᵘᵈᵘ ᴮᵒᵀ ᴳᴵᵀ & ᵂᴬ ᴳᴿᵁᴾ ᴸᴵᴺᵏ  https://github.com/I4MBetAZ/PBA-BoT-V1
+    𝙱𝚘𝚃 𝙶𝚒𝚝 & 𝚆𝙰 𝙶𝚛𝚘𝚞𝚙 𝙻𝚒𝚗𝚔🤸🏼
+  https://github.com/I4MBetAZ/PBA-BoT-V1
   WhatsApp Group link
   https://chat.whatsapp.com/D3fhKLgNybo7RP4TEKgrFc
- Subscribe PBA MoDs
-`https://www.youtube.com/channel/UCxkxhbcHQb2B8JH3j-UVArQ/videos    await conn.send2ButtonImg(m.chat, await (await fetch("https://telegra.ph/file/24205ebf7697a6519e519.png")).buffer(), haruno, 'Thakkudu WA BoT', 'KIdu😝', '.menu', 'Nice😉', '.menu', m)}
+ Watch Tutorial Video
+`.trim(https://www.youtube.com/channel/UCxkxhbcHQb2B8JH3j-UVArQ)
+    await conn.send2ButtonImg(m.chat, await (await fetch("https://telegra.ph/file/24205ebf7697a6519e519.png")).buffer(), haruno, 'Thakkudu BoT', 'Kidu🤹🏻‍♂️', '.menu', 'HeHe🤺', '.menu', m)
+}
 handler.tags = ['main']
 handler.help = ['git']
 handler.command = /^(git)$/i
