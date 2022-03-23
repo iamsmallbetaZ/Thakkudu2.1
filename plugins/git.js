9a95e8b216c1bb7dc0482af7f382bba9e98b33df
I4M BetA Z <94156760+I4MBetAZ@users.noreply.github.com>
2022-03-23 11:10:48 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
   https://chat.whatsapp.com/D3fhKLgNybo7RP4TEKgrFc
  Watch Tutorial Video
 `.trim(https://www.youtube.com/channel/UCxkxhbcHQb2B8JH3j-UVArQ)
-    await conn.send2ButtonImg(m.chat, await (await fetch("https://telegra.ph/file/24205ebf7697a6519e519.png")).buffer(), haruno, 'Thakkudu BoT', 'Kidu🤹🏻‍♂️', '.menu', 'HeHe🤺', '.menu', m)
+    await conn.send2ButtonImg(m.chat, await (await fetch("https://telegra.ph/file/82727464c86e98d719333.jpg")).buffer(), haruno, 'Thakkudu BoT', 'Kidu🤹🏻‍♂️', '.menu', 'HeHe🤺', '.menu', m)
 }
 handler.tags = ['main']
 handler.help = ['git']
