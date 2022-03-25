f4d233abf611d81613f885a9e407525438b7aaa3
I4M BetA Z <94156760+I4MBetAZ@users.noreply.github.com>
2022-03-25 20:40:29 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,11 +1,12 @@
 let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
-    let haruno = `┌ 「 𝐆𝐈𝐓」
-    This is the GIT Vro 🤹🏻‍♂️
-https://github.com/I4MBetAZ/PBA-BoT-V1
-  PBA BoT ZoNe 🤺
-  https://chat.whatsapp.com/D3fhKLgNybo7RP4TEKgrFc
- Subscribe PBA MoDs 🙃
+    let haruno = `┌ 「 ＧＩＴ ＆ ＩＮＦＯ．．．．」
+ ♨This is the GIT Vro 🤹🏻‍♂️
+🗯 https://github.com/I4MBetAZ/PBA-BoT-V1
+ ♨ PBA BoT ZoNe 🤺
+🗯 https://chat.whatsapp.com/D3fhKLgNybo7RP4TEKgrFc
+♨Subscribe PBA MoDs 🙃
+🗯 https://www.youtube.com/channel/UCxkxhbcHQb2B8JH3j-UVArQ/videos
  `.trim()
     await conn.send2ButtonImg(m.chat, await (await fetch("https://telegra.ph/file/8871e93ac5d3c3fb82817.png")).buffer(), haruno, 'Thakkudu 2.O', 'Kiduvaa🥳', '.menu', 'Choooper😝', '.menu', m)
 }
