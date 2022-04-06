d78fc235a04ddd6403178bce96d3cf8ec4dc49b9
I4M BetA Z <94156760+I4MBetAZ@users.noreply.github.com>
2022-04-06 13:02:31 +0530

    Add files via upload

---
+++ plugins/pussy.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, 'https://api.xteam.xyz/randomimage/pussy?APIKEY=4a8b9aba75823076', '', '', m)
+}
+handler.help = ['pussy']
+handler.tags = ['NSFW']
+handler.command = /^(pussy)$/i
+
+handler.limit = true
+handler.nsfw = true
+handler.group = true
+
+module.exports = handler
