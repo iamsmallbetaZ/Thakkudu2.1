f33d5f1edd10bec6733f9e0da5f0698f1d37fcf5
I4M BetA Z <94156760+I4MBetAZ@users.noreply.github.com>
2022-04-06 12:41:11 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -2,15 +2,15 @@ const { igdl } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `url salah, perintah ini untuk mengunduh post/reel/tv`
+  if (!args[0]) throw `Where is the url?\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
+  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `paste uer url post/reel/tv`
 
   igdl(args[0]).then(async res => {
     let igdl = JSON.stringify(res)
     let json = JSON.parse(igdl)
     await m.reply(global.wait)
     for (let { downloadUrl, type } of json) {
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© shiraori', m)
+      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© Thakkudu2.O', m)
     }
   })
 
