2cf0858a5f81d46aa76a3ee3da4007b7c512bf89
I4M BetA Z <94156760+I4MBetAZ@users.noreply.github.com>
2022-04-06 12:30:48 +0530

    Update igstory.js

--- plugins/igstory.js
+++ plugins/igstory.js
@@ -2,15 +2,15 @@ const { igstory } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `uhm.. where's the username?\n\nexample:\n\n${usedPrefix + command} leomessi`
-  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `wrong username`
+  if (!args[0]) throw `uhm.. username nya mana?\n\ncontoh:\n\n${usedPrefix + command} wakaba.hiiro`
+  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `username salah`
 
   igstory(args[0]).then(async res => {
     let igs = JSON.stringify(res)
     let json = JSON.parse(igs)
     await m.reply(global.wait)
     for (let { downloadUrl, type } of json)
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), watermark, m)
+      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© Thakkudu2.O', m)
 
   })
 
